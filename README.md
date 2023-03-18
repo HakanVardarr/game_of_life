4c0e4400dc8ce5fe3f1ed631a227cdafe3d3fdc8
Hakan Vardar <hakovardo123@gmail.com>
2023-03-18 13:04:43 +0300

    Added feature to load cells from file

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # game_of_life
+
 Conway's Game of Life
+
+## Usage:
+
+cargo run "height (30)" "width (100)" "example.txt"
 
 [![asciicast](https://asciinema.org/a/lIYdcFU5S8G2MSgugNXdVsLt1.svg)](https://asciinema.org/a/lIYdcFU5S8G2MSgugNXdVsLt1)
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ use game_of_life::Board;
 use game_of_life::GameOfLife;
 
 fn main() {
-    let mut game = GameOfLife::new(Board::new(30, 100));
-    game_init(&mut game);
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 4 {
+        eprintln!("ERROR: Not enough arguments");
+        std::process::exit(0);
+    }
+
+    let height = args[1].parse::<usize>().unwrap();
+    let width = args[2].parse::<usize>().unwrap();
+
+    let mut game = GameOfLife::new(Board::new(height, width));
+    game_init(&mut game, &args[3]);
 }
